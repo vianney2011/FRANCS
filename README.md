3d0aa6d582b62e6b0fa1187ef32c4e8d5f8c703f
vianney2011 <vianney.menard@gmail.com>
2023-08-21 15:04:54 +0200

    test

---
+++ README.md
@@ -0,0 +1,3 @@
+ALL rights are reserved to VIANNEY MENARD.
+
+Built in the context of a school project for EPSI
---
+++ client/src/components/RightSidebar/RightSidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RightSidebar = () => {
+  return (
+    <div className="p-3 bg-slate-100 rounded-lg mx-13 space-y-5">
+      <h2 className="font-medium">Trending</h2>
+      <p className="font-bold">#FrancsOuGab</p>
+      <p className="font-bold">#FreeAndrew</p>
+      <p className="font-bold">#HunterLaptop</p>
+      <p className="font-bold">#BackToKupiansk</p>
+    </div>
+  );
+};
+
+export default RightSidebar;
